8d33e89ec2aa97f3e77638ae651a4601a7c4ea86
Utilizater <polevoynikita@yandex.ru>
2019-09-03 21:29:42 -0400

    Initial Commit

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {View, StyleSheet, Text} from "react-native";
+
+const IndexScreen = () => {
+
+  return(
+    <View>
+      <Text>Index Screen!!!</Text>
+    </View>
+  );
+}
+
+const styles = {};
+
+export default IndexScreen;
